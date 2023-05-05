a5183801a7f192be69580c2dc9ef027d3b438e0f
Kabunar <129909891+Kabunar@users.noreply.github.com>
2023-05-05 12:26:26 +1200

    Basics
    https://www.youtube.com/watch?v=5FgCjm63uko

---
+++ main.py
@@ -0,0 +1,13 @@
+#Choose a number
+get_number = int(input("Choose a number? "))
+
+
+#Multiply the number by five
+times_five = get_number*5
+
+#output the result
+answer = "{} times five is equal to {}.".format(get_number, times_five)
+
+
+#print the answer
+print(answer)
